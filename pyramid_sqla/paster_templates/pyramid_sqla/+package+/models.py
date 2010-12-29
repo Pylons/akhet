074ab5381ab7f4e0dd8b65be4e0bdc48cdd0069a
Mike Orr <sluggoster@gmail.com>
2010-12-28 17:31:04 -0800

    Move declarative base to application model to avoid complexities in library.

--- pyramid_sqla/paster_templates/pyramid_sqla/+package+/models.py
+++ pyramid_sqla/paster_templates/pyramid_sqla/+package+/models.py
@@ -1,9 +1,12 @@
 import pyramid_sqla as psa
 import sqlalchemy as sa
+import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
 import transaction
 
-#class MyModel(psa.Base):
+Base = declarative.declarative_base()
+
+#class MyModel(Base):
 #    __tablename__ = 'models'
 #    id = sa.Column(sa.Integer, primary_key=True)
 #    name = sa.Column(sa.Unicode(255), nullable=False)
