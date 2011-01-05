854228af442c911213c589c748a0ac685290fef8
Mike Orr <sluggoster@gmail.com>
2011-01-05 12:27:49 -0800

    More tests. (TestDeclarativeBase.test1 doesn't work.)

--- pyramid_sqla/tests/test.py
+++ pyramid_sqla/tests/test.py
@@ -68,5 +68,76 @@ class TestAddEngine(PyramidSQLATestCase):
             "stats.url": self.db2.url,
             "foo": "bar"}
         default = psa.add_engine(settings)
-        stats = psa.add_engine(settings, name="stats", prefix="stats")
+        stats = psa.add_engine(settings, name="stats", prefix="stats.")
+        # Can we retrieve the engines?
+        self.assertIs(psa.get_engine(), default)
+        self.assertIs(psa.get_engine("default"), default)
+        self.assertIs(psa.get_engine("stats"), stats)
+        # Are the session binding and base binding set correctly?
+        self.assertIs(psa.get_session().bind, default)
+        self.assertIs(psa.get_base().metadata.bind, default)
+
+    def test_multiple_engines_without_default(self):
+        settings = {
+            "db1.url": self.db1.url,
+            "db2.url": self.db2.url,
+            "foo": "bar"}
+        db1 = psa.add_engine(settings, name="db1", prefix="db1.")
+        db2 = psa.add_engine(settings, name="db2", prefix="db2.")
+        # Can we retrieve the engines?
+        self.assertIs(psa.get_engine("db1"), db1)
+        self.assertIs(psa.get_engine("db2"), db2)
+        # There should be no default engine
+        self.assertIs(psa.get_session().bind, None)
+        self.assertIs(psa.get_base().metadata.bind, None)
+        self.assertRaises(RuntimeError, psa.get_engine)
+
+class TestConfigSession(PyramidSQLATestCase):
+    def test1(self):
+        engine1 = psa.add_engine(url=self.db1.url, name="db1")
+        engine2 = psa.add_engine(url=self.db2.url, name="db2")
+        psa.config_session(autocommit=False, autoflush=True, twophase=False)
+        Session = psa.get_session()
+        sess = Session()
+        self.assertEqual(sess.autocommit, False)
+        self.assertEqual(sess.autoflush, True)
+        self.assertEqual(sess.twophase, False)
+        # Now delete the session, change an option, and recreate it
+        Session.remove()
+        psa.config_session(twophase=True)
+        sess = Session()
+        self.assertEqual(sess.twophase, True)
+
+class TestDeclarativeBase(PyramidSQLATestCase):
+    def test1(self):
+        Base = psa.get_base()
+        class Person(Base):
+            __tablename__ = "people"
+            id = sa.Column(sa.Integer, primary_key=True)
+            first_name = sa.Column(sa.Unicode(100), nullable=False)
+            last_name = sa.Column(sa.Unicode(100), nullable=False)
+        psa.add_engine(url=self.db1.url)
+        Base.metadata.create_all()
+        fred = Person(id=1, first_name=u"Fred", last_name=u"Flintstone")
+        wilma = Person(id=2, first_name=u"Wilma", last_name=u"Flintstone")
+        barney = Person(id=3, first_name=u"Barney", last_name=u"Rubble")
+        betty = Person(id=4, first_name=u"Betty", last_name=u"Rubble")
+        sess = psa.get_session()()
+        sess.add_all([fred, wilma, barney, betty])
+        sess.commit()
+        sess.expunge_all()
+        del fred, wilma, barney, betty
+        # Can we get back a record?
+        barney2 = sess.query(Person).get(3)
+        self.assertEqual(barney2.id, 3)
+        self.assertEqual(barney2.first_name, u"Barney")
+        self.assertEqual(barney2.last_name, u"Rubble")
+        sql = sa.select([Person.first_name])
+        # Can we iterate the first names in reverse alphabetical order?
+        q = sess.query(Person.first_name).order_by(Person.first_name.desc())
+        result = [x.first_name for x in q]
+        control = [u"Wilma", u"Fred", u"Betty", u"Barney"]
+        self.assertEqual(result, control)
+
+
 
