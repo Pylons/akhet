1678d82abd718b16f2d08746f68f47394ad9720c
sluggoster <sluggoster@gmail.com>
2011-02-21 12:01:40 -0800

    Consolidate copyright statements.

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
-##############################################################################
-#
-# Copyright (c) 2010 Agendaless Consulting and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the BSD-like license at
-# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
-# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
-# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
-# FITNESS FOR A PARTICULAR PURPOSE
-#
-##############################################################################
-
+"""Akhet installation script.
+"""
 import os
 
 from setuptools import setup
