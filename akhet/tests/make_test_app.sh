eec51fa5b662587c051daca9ea87fa6d50cae8a1
sluggoster <sluggoster@gmail.com>
2011-03-12 23:45:14 -0800

    akhet/tests/make_test_app.sh for testing.

---
+++ akhet/tests/make_test_app.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+# Make a test app 'Zzz' in the current directory and run it.
+
+APPNAME=Zzz
+
+if [ -e $APPNAME ] ;then
+    echo -n 1>&2 "Error: test app '$APPNAME' exists, aborting."
+    echo 1>&2 "  Suggested command line:"
+    echo 1>&2 "rm -rf $APPNAME && bash $0"
+    exit 1
+fi
+
+paster create -t akhet $APPNAME
+cd $APPNAME
+paster serve development.ini
