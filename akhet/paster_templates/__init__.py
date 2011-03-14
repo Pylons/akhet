cddddeebf318fc4163e53beabf70b56451991cdc
sluggoster <sluggoster@gmail.com>
2011-03-13 22:21:28 -0700

    Ask whether to configure SQLAlchemy; switch to repoze_tm transaction manager.

--- akhet/paster_templates/__init__.py
+++ akhet/paster_templates/__init__.py
@@ -1,3 +1,5 @@
+from paste.deploy.converters import asbool
+from paste.script.templates import var
 from paste.util.template import paste_script_template_renderer
 from pyramid.paster import PyramidTemplate
 
@@ -5,12 +7,13 @@ class AkhetProjectTemplate(PyramidTemplate):
     _template_dir = "akhet"
     summary = "A Pylons-like Pyramid project"
     template_renderer = staticmethod(paste_script_template_renderer)
+    vars = [
+        var("sqlalchemy", "Include SQLAlchemy configuration? (y/n)",
+            default=True),
+        ]
 
     def pre(self, command, output_dir, vars): # pragma: no cover
         """Called before template is applied."""
         PyramidTemplate.pre(self, command, output_dir, vars)
-        package_logger = vars["package"]
-        if package_logger == "root":
-            # Rename the app logger in the rare case a project is named 'root'
-            package_logger = "app"
-        vars["package_logger"] = package_logger
+        # Superclass sets vars 'random_string' and 'package_logger'.
+        vars["sqlalchemy"] = asbool(vars["sqlalchemy"])
