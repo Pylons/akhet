be136aa098d5106358aca253ad9ce39af6819c2e
sluggoster <sluggoster@gmail.com>
2011-03-30 23:35:51 -0700

    Bump version to 1.0. (not released yet)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ entry_points = """
 """
 
 setup(name="Akhet",
-      version="1.0b2",
+      version="1.0",
       description="Pyramid application templates inspired by Pylons 1.",
       long_description=README,
       #long_description=README + "\n\n" +  CHANGES,
