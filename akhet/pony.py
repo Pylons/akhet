85034d771c2a5e7cd798c5076da16db630f1079e
Mike Orr <sluggoster@gmail.com>
2012-02-12 22:44:40 -0800

    Delete Paste middleware code from pony module.

--- akhet/pony.py
+++ akhet/pony.py
@@ -69,35 +69,3 @@ def view(request):
     animal = zlib.decompress(data)
     html = TEMPLATE.format(animal=animal, url=url, link=link, home=home)
     return Response(html)
-
-
-class PonyMiddleware(object):
-
-    def __init__(self, application):
-        self.application = application
-
-    def __call__(self, environ, start_response):
-        path_info = environ.get('PATH_INFO', '')
-        if path_info == '/pony':
-            url = construct_url(environ, with_query_string=False)
-            if 'horn' in environ.get('QUERY_STRING', ''):
-                data = UNICORN
-                link = 'remove horn!'
-            else:
-                data = PONY
-                url += '?horn'
-                link = 'add horn!'
-            msg = data.decode('base64').decode('zlib')
-            msg = '<pre>%s\n<a href="%s">%s</a></pre>' % (
-                msg, url, link)
-            start_response('200 OK', [('content-type', 'text/html')])
-            return [msg]
-        else:
-            return self.application(environ, start_response)
-
-def make_pony(app, global_conf):
-    """
-    Adds pony power to any application, at /pony
-    """
-    return PonyMiddleware(app)
-
